a17de8403cba73995c36d4cccff88c864467b073
mishra-pankaj <immishrapankaj@gmail.com>
2025-06-22 23:10:55 +0530

    board layout logic

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,1 +1,43 @@
+
+
 const socket = io()
+const chess = new Chess()
+const boardElement = document.querySelector(".chessboard")
+
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+
+const renderBoard = () =>{
+    const board = chess.board();
+    boardElement.innerHTML=""
+    board.forEach((row, rowindex)=>{
+        row.forEach((square, squareindex)=>{
+            const squareElement = document.createElement("div")
+            squareElement.classList.add("square",
+                (rowindex + squareindex)%2 ===0? "light" : "dark"
+
+            )
+            squareElement.dataset.row = rowindex;
+            squareElement.dataset.col = squareindex
+
+           if (square) {
+            const pieceElement = document.createElement("div")
+            pieceElement.classList.add(
+                "piece",
+                square.color === "w" ? "white" : "black"   
+            )
+            pieceElement.innertext = ""
+           } 
+        })
+        
+    })
+}
+
+const handleMove = ()=>{}
+
+const getPieceUnicode =()=>{}
+
+renderBoard()
